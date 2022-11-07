ce0d26e933ead44283b7cfd6af8e593b36b0f9c5
luis reyes <josreyes865@gmail.com>
2022-11-07 11:08:42 -0600

    Ultmos pasos de tercera entrega

--- script.js
+++ script.js
@@ -1,3 +1,113 @@
+// script banner bienvenida
+
+let checbox_iniciar = document.getElementById("checbox_iniciar");
+let checbox_registrarse = document.getElementById("checbox_registrarse")
+
+checbox_registrarse.addEventListener("click" , registrarse);
+checbox_iniciar.addEventListener("click" , iniciar_sesion);
+
+function iniciar_sesion(){
+    let form = document.createElement("form");
+    form.innerHTML = 
+                        `<input type="text" placeholder="Usuario">
+                        <input type="password" placeholder="Contraseña">
+                        <button class="btn btn-primary" id="btn_ingresar">Ingresar</button>
+                        <a href="index.html">Regresar</a>`
+
+    let banner_bienvenida = document.getElementById("banner_bienvenida");
+
+    banner_bienvenida.append(form);
+
+    let input_bienvenida = document.getElementById("inputs_bienvenida");
+    input_bienvenida.remove();
+}
+
+function registrarse(){
+    let form = document.createElement("form");
+    form.innerHTML = `
+                        <input type="text" placeholder="Nombre" id="nombre_usuario">
+                        <input type="text" placeholder="Usuario" id="usuario">
+                        <input type="password" placeholder="Contraseña" id="password">
+                        <input type="number" placeholder="Edad"> 
+                        <button class="btn btn-primary" id="btn_registrar">Registrarse</button>
+                        <a href="index.html">Regresar</a>`
+
+    let mensaje_bienvenida = document.querySelector("h4").textContent = "Bienvenido. Hablanos un poco de ti";
+
+
+    let banner_bienvenida = document.getElementById("banner_bienvenida");
+    banner_bienvenida.append(form);
+
+    let input_bienvenida = document.getElementById("inputs_bienvenida");
+    input_bienvenida.remove();
+
+    let btn_registro = document.getElementById("btn_registrar");
+
+    btn_registro.addEventListener("click" , alta_usuario);
+}
+
+//  fin script banner de bienvenida
+
+// Inicia script para registro / inicio sesion 
+
+let arreglo_usarios = [];
+
+function alta_usuario(){
+    let usuario = document.getElementById("usuario");
+    let pass = document.getElementById("password");
+    let nombre_usuario = document.getElementById("nombre_usuario");
+
+    let objeto_usuario = {
+        nombre: nombre_usuario.value,
+        pass: pass.value,
+        usuario: usuario.value
+    }
+
+    arreglo_usarios.push(objeto_usuario);
+
+    if(localStorage.length != 0){
+        let recuperado_arreglo = localStorage.getItem("usuarios");
+        recuperado_arreglo = JSON.parse(recuperado_arreglo);
+
+        for(let usuario_arreglo of recuperado_arreglo){
+            
+            if(objeto_usuario.usuario == usuario_arreglo.usuario){
+                let p = document.createElement("p");
+                p.innerHTML = `<p>Este usuario ya está registrado</p>`
+
+                let alerta_usuario_registrado = document.getElementById("banner_bienvenida");
+                alerta_usuario_registrado.append(p);
+
+            }
+            else{
+                let p = document.createElement("p");
+                p.innerHTML = `<p>Registro exitoso</p>`
+
+                let registro_exitoso = document.getElementById("banner_bienvenida");
+                registro_exitoso.append(p);
+
+                let input_bienvenida = document.getElementById("inputs_bienvenida");
+                input_bienvenida.remove();
+            }
+        }
+    }
+    else{
+        let arreglo_json = JSON.stringify(arreglo_usarios);
+        localStorage.setItem("usuarios" , arreglo_json);
+
+        let p = document.createElement("p");
+                p.innerHTML = `<p>Registro exitoso</p>`
+
+                let registro_exitoso = document.getElementById("banner_bienvenida");
+                registro_exitoso.append(p);
+
+                let input_bienvenida = document.getElementById("inputs_bienvenida");
+                input_bienvenida.remove();
+
+    }
+}
+
+// Fin de script para registro / inicio de sesion 
 let carrito = [];
 
 let btn_comprar = document.getElementsByClassName("btn-comprar");
@@ -27,47 +137,16 @@ function agregar_al_carrito(e){
 
     carrito.push(producto);
 
-    // mostrar_carrito(producto);
-
-     // CONVERTIR A JSON PARA GUARDAR EN LOCALSTORAGE, DESPUES CON OTRA FUNCION DE REDERIZADO LLAMAS EL CARRITO DE JSON Y LO CONVIERTES PARA DESPUES RENDER
-
-    let carrito_json = JSON.stringify(carrito);
-    let producto_sesionstorage = sessionStorage.setItem("carrito" , carrito_json);
-
-    convertir_carrito(producto_sesionstorage);
-
-    // console.log(carrito_json);
+    renderizar_carrito(producto);
 }
 
-function convertir_carrito(producto_sesionstorage){
-
-    for ( let i = 0; i < sessionStorage.length; i++){
-        let clave = sessionStorage.key(i);
-
-        let objeto_carrito_json = sessionStorage.getItem(clave);
-        let array_objetos_carrito = JSON.parse(objeto_carrito_json);
-
-        mostrar_carrito(array_objetos_carrito); 
-    }
-}
-
-function mostrar_carrito(array_objetos_carrito){
-
-    for(let i = 0; i < array_objetos_carrito.length; i++){
-
-        let objeto = array_objetos_carrito[i];
-
-        renderizar_carrito(objeto);
-    }
-}
-
-function renderizar_carrito(objeto){
+function renderizar_carrito(producto){
 
     let fila = document.createElement("tr");
-    fila.innerHTML = `<td><img src="${objeto.img}"></td>
-                <td>${objeto.nombre}</td>
-                <td>${objeto.precio}</td>
-                <td>${objeto.cantidad}</td>
+    fila.innerHTML = `<td><img src="${producto.img}"></td>
+                <td>${producto.nombre}</td>
+                <td>${producto.precio}</td>
+                <td>${producto.cantidad}</td>
                 <td><button class="btn btn-danger borrar_elemento">Borrar</td>`;
 
     let tabla = document.getElementById("tbody");
@@ -111,5 +190,3 @@ function abrir_carrito(){
 
     }
 }
-
-sessionStorage.clear();
